0ccd733ea67f37ca14b2ed9d9c5ba5ace9b5c63f
Rayan <rayanneogi2005@gmail.com>
2026-05-05 19:25:39 +0530

    2nd version of data saved

--- dvc_mlops/code_dvc.py
+++ dvc_mlops/code_dvc.py
@@ -5,7 +5,9 @@ data = {'Name': ['ayan','krish','bill'],
         'Age': [20,25,30],
         'City':['Chd','wb','hr']}
 df = pd.DataFrame(data)
-
+# adding a new row for v2 of data
+new_row_loc = {'Name':"Rah","Age":20,"City": "Chd"}
+df.loc[len(df.index)] = new_row_loc 
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok = True)
 
